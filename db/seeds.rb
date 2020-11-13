d412b2aa3b36ad2f49ead55e2e82bb021c6e58c3
Raghu Betina <raghu@firstdraft.com>
2020-11-13 13:24:33 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create [
+  {
+    name: 'alice',
+    born_on: 'January 1st 2001',
+    time_of_birth: '1:00am',
+    born_at: 'January 1st 2001 1:00am',
+    age: 19,
+    height_in_inches: 65.1
+  },
+  {
+    name: 'bob',
+    born_on: 'February 2nd 2002',
+    time_of_birth: '2:00am',
+    born_at: 'February 2nd 2002 2:00am',
+    age: 18,
+    height_in_inches: 66.1
+  },
+  {
+    name: 'carol',
+    born_on: 'March 3rd 2003',
+    time_of_birth: '3:00am',
+    born_at: 'March 3rd 2003 3:00am',
+    age: 17,
+    height_in_inches: 67.1
+  },
+  {
+    name: 'doug',
+    born_on: 'April 4th 2004',
+    time_of_birth: '4:00am',
+    born_at: 'April 4th 2004 4:00am',
+    age: 16,
+    height_in_inches: 68.1
+  }
+]
